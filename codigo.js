1c1d49c21403e1fb655eff4908db6e57e879d8ff
DiegoLibonati <diego.libonati1998@gmail.com>
2022-11-06 21:41:28 -0300

    html semantic, new css, new code js

--- codigo.js
+++ codigo.js
@@ -1,31 +1,50 @@
-const btns = document.querySelectorAll(".buttons");
-const newText = document.querySelector(".text");
+const btnsTab = document.querySelectorAll(
+  ".tabs_container_text_buttons_button"
+);
+const tabText = document.getElementById("tab-text");
+const tabImage = document.getElementById("tab-image");
 
+const changeTabInformation = (e) => {
+  const btnTabId = e.target.id;
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", (e)=>{
-        let myActualButton = e.target.id;
+  changeClassActiveButton(e);
 
-        if (myActualButton == "btnhistory"){
-            newText.innerHTML = "La historia de este lugar es xD"
-        } else if (myActualButton == "btnvision"){
-            newText.innerHTML = "La vision es xD"
-        } else {
-            newText.innerHTML = "Los goals son xd"
-        }
-    });
-});
+  if (btnTabId === "history") {
+    tabText.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia.";
+    tabImage.src =
+      "https://www.absolutviajes.com/wp-content/uploads/2008/11/arquitectura-china-antigua.png";
+    return;
+  }
 
-document.body.addEventListener("keydown", (e)=>{
-    let myActualButton = e.target.id;  
-    if (e.key == "Tab"){
-        if (myActualButton == ""){
-            newText.innerHTML = "La historia de este lugar es xD"
-        } else if (myActualButton == "btnhistory"){
-            newText.innerHTML = "La vision es xD"
-        } else if (myActualButton == "btnvision"){
-            newText.innerHTML = "Los goals son xd"
-            
-        }
+  if (btnTabId === "vision") {
+    tabText.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia.";
+    tabImage.src = "https://img.lovepik.com/photo/50131/9815.jpg_wh860.jpg";
+    return;
+  }
+
+  if (btnTabId === "goals") {
+    tabText.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, mollitia.";
+    tabImage.src =
+      "http://valeriavasquez.weebly.com/uploads/4/8/7/7/48775221/1305460_orig.jpg";
+    return;
+  }
+};
+
+const changeClassActiveButton = (event) => {
+  const btnPressed = event.target;
+
+  btnsTab.forEach((btnTab) => {
+    if (btnTab.classList.contains("isActive")) {
+      btnTab.classList.remove("isActive");
     }
+  });
+
+  btnPressed.classList.add("isActive");
+};
+
+btnsTab.forEach((btnTab) => {
+  btnTab.addEventListener("click", (e) => changeTabInformation(e));
 });
