1a1e26c44e0fcfcc60afcdc27b2ab4b9c23e0bd1
Die <dlibonati@leafnoise.io>
2025-01-24 13:26:33 -0300

    feat: BEM CSS

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,7 +1,5 @@
 export const getElements = () => ({
-  btnsTab: document.querySelectorAll(
-    ".tabs__list__btns__btn"
-  ) as NodeList,
+  btnsTab: document.querySelectorAll(".tabs-wrapper__list-tab") as NodeList,
   tabText: document.getElementById("tab-text") as HTMLParagraphElement,
   tabImage: document.getElementById("tab-image") as HTMLImageElement,
 });
